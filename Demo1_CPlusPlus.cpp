64b9bc0d16b6a28079f6f88cd5d0f22c1533d77f
Bojan B <83359157+bonjadev@users.noreply.github.com>
2023-05-09 11:21:33 +0200

    First commit

---
+++ Demo1_CPlusPlus.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	int x, y;
+	int sum;
+	cout << "Unesite broj: ";
+	cin >> x;
+	cout << "Unesite drugi broj: ";
+	cin >> y;
+	sum = x + y;
+	cout << "Zbir je: " << sum;
+	return 0;
+}
